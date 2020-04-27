08ae576cc2880b1e81275206af5b023f1412d3e7
hsn <hsn@hsns-MacBook-Pro.local>
2020-04-27 05:55:35 +0530

    added cart module in app

--- myshop/myshop/myshop/settings.py
+++ myshop/myshop/myshop/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'shop.apps.ShopConfig',
     'cart.apps.CartConfig',
+    'orders.apps.OrdersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -64,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
             ],
         },
     },
