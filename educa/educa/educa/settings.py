312b71cc673b53db2bf3a094e31054fb04622f30
hsn <hsn@hsns-MacBook-Pro.local>
2020-05-12 09:05:39 +0530

    configured channel and implemented chat room group

--- educa/educa/educa/settings.py
+++ educa/educa/educa/settings.py
@@ -154,11 +154,16 @@ REST_FRAMEWORK = {
 
 ASGI_APPLICATION = 'educa.routing.application'
 
-# CHANNEL_LAYERS = {
-#     'default': {
-#         'BACKEND': 'channels_redis.core.RedisChannelLayer',
-#         'CONFIG': {
-#             'hosts': [('127.0.0.1', 6379)],
-#         },
-#     },
-# }
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('127.0.0.1', 6379)],
+        },
+    },
+}
+
+
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 0
